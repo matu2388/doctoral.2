8528e032e2686f87a115ab68e9f730e9696341cb
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 22:41:07 +0000

    .

--- config/tile-layer/data.js
+++ config/tile-layer/data.js
@@ -1,3 +1,23 @@
+const ATRIBUTIONS_LIST = {
+    CartoDb: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    CyclOSM:
+        '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    EsriWorldImagery:
+        "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    EsriWorldTopoMap:
+        "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
+    OSM: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    OSMHot:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+    OpenTopo:
+        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    Stadia:
+        '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+    ThunderForest:
+        '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    USGS: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+};
+
 export const tileLayers = {
     baseLayers: {
         esri: {
--- src/assets/script.js
+++ src/assets/script.js
@@ -1,15 +1,17 @@
-import { sitios } from '../../dist/assets/data/markers/sitios'
+import { sitios } from '../../dist/assets/data/markers/sitios';
+import { tileLayerSelect } from '../../config/tile-layer/function';
+import { tileLayers} from '../../config/tile-layer/data';
 
 document.addEventListener("DOMContentLoaded", function() {
-    const mymap = new L.map('map').setView([0, 0], 13);
+    const map = new L.map('map').setView([0, 0], 13);
 
 tileLayerSelect(tileLayers.baseLayers.esri.worldImagery.map, {
     attribution: tileLayers.baseLayers.esri.worldImagery.atribution
-}).addTo(mymap);
+}).addTo(map);
 
     // Carga dinámica de los marcadores
 sitios.forEach((point) => {
-    const markerInstance = marker([point.lat, point.lon]).addTo(mymap);
+    const markerInstance = marker([point.lat, point.lon]).addTo(map);
     markerInstance.bindPopup(`<b>${point.name}</b>`); // Agregar pop-up con el nombre del sitio
 });
 
