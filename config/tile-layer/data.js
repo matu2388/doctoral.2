d06e1db915c44e8405fc157acb604f51360743a4
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 16:29:17 -0300

    Update data.js

--- config/tile-layer/data.js
+++ config/tile-layer/data.js
@@ -1,10 +1,14 @@
 export const tileLayers = {
     baseLayers: {
         esri: {
+            worldStreetMap: {
+                map: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+                atribution: ATRIBUTIONS_LIST.EsriWorldTopoMap
+            },
             worldImagery: {
-                map: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-                attribution: 'Tiles © Esri'
+                map: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+                atribution: ATRIBUTIONS_LIST.EsriWorldImagery
             }
         }
     }
-};
+}
