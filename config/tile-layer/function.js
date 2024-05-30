3750898d82651fb7e71dae19e3071e5f4939c636
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 16:25:03 -0300

    .

--- config/tile-layer/function.js
+++ config/tile-layer/function.js
@@ -1,30 +1,5 @@
-import { TileLayer, tileLayer } from "leaflet";
-import { tileLayers, tileLayersWMS } from "./data";
-import { ITilerLayerOptions } from "./option.interface";
+import L from 'leaflet';
 
-export const tileLayerSelect = (
-    layer = tileLayers.baseLayers.esri.worldImagery.map,
-    options = {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: tileLayers.baseLayers.esri.worldImagery.atribution,
-    }
-) => {
-    return tileLayer(layer, options);
-};
-
-
-export const tileLayerWMSSelect = (
-    service = tileLayersWMS.mundialis.baseUrl, 
-    options = {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: tileLayers.baseLayers.esri.worldImagery.atribution,
-        layers: tileLayersWMS.mundialis.layers.dark,
-        format: 'image/png',
-        transparent: true,
-    }   
-) => {
-    return tileLayer.wms(service, options);
-};
-
+export function tileLayerSelect(url, options) {
+    return L.tileLayer(url, options);
+}
