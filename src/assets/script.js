dd228ee10b7dde6fd64db8ba7d9608d4a86c867c
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 20:48:44 +0000

    .

---
+++ src/assets/script.js
@@ -0,0 +1,19 @@
+import { sitios } from '../../dist/assets/data/markers/sitios'
+
+document.addEventListener("DOMContentLoaded", function() {
+    const mymap = new Map('map').setView([0, 0], 13);
+
+tileLayerSelect(tileLayers.baseLayers.esri.worldImagery.map, {
+    attribution: tileLayers.baseLayers.esri.worldImagery.atribution
+}).addTo(mymap);
+
+    // Carga dinámica de los marcadores
+sitios.forEach((point) => {
+    const markerInstance = marker([point.lat, point.lon]).addTo(mymap);
+    markerInstance.bindPopup(`<b>${point.name}</b>`); // Agregar pop-up con el nombre del sitio
+});
+
+mymap.fitBounds(sitios.map(point => [point.lat, point.lon]));
+
+});
+
