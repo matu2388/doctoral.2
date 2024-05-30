927e2fd81d2bdc30d7d98b55e1f9e171c6970608
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 22:45:58 +0000

    .

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,9 +1,10 @@
+import { Map, marker, point } from 'leaflet';
 import { sitios } from '../../dist/assets/data/markers/sitios';
 import { tileLayerSelect } from '../../config/tile-layer/function';
 import { tileLayers} from '../../config/tile-layer/data';
 
 document.addEventListener("DOMContentLoaded", function() {
-    const map = new L.map('map').setView([0, 0], 13);
+    const map = new L.map('map').setView([-26.988189888230163, -66.73794507576763], 13);
 
 tileLayerSelect(tileLayers.baseLayers.esri.worldImagery.map, {
     attribution: tileLayers.baseLayers.esri.worldImagery.atribution
@@ -15,7 +16,7 @@ sitios.forEach((point) => {
     markerInstance.bindPopup(`<b>${point.name}</b>`); // Agregar pop-up con el nombre del sitio
 });
 
-mymap.fitBounds(sitios.map(point => [point.lat, point.lon]));
+map.fitBounds(sitios.map(point => [point.lat, point.lon]));
 
 });
 
