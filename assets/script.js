12f42a976a084e4182b47361dd4719548aad6c7a
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 16:14:17 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
-import L from 'leaflet';
-import { tileLayerSelect } from '../config/tile-layer/function';
-import { tileLayers } from '../config/tile-layer/data';
+document.addEventListener("DOMContentLoaded", function() {
+    var map = L.map('map').setView([-26.986111, -66.733333], 13);
 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
 
-const map = new L.Map('map').setView([-26.986111, -66.733333], 13);
+    L.marker([-26.988259032403032, -66.73797058911501]).addTo(map).bindPopup(`
+        <h5 class ="MRC_title">Museo Rural Comunitario</h5>
+        <p>MUSEO RURAL COMUNITARIO manejado por la comunidad del valle de El Bolsón, Belén, Catamarca.</p>
+        <img src="https://shorturl.at/ZRYSp">
+    `);
 
-tileLayerSelect(tileLayers.baseLayers.esri.worldImagery.map, {
-    attribution: tileLayers.baseLayers.esri.worldImagery.atribution
-}).addTo(map);
-
-const museoMarker = L.marker([-26.988259032403032, -66.73797058911501]).addTo(map).bindPopup(`
-    <h5 class ="MRC_title">Museo Rural Comunitario</h5>
-    <p>MUSEO RURAL COMUNITARIO manejado por la comunidad del valle de El Bolsón, Belén, Catamarca.</p>
-    <img src="https://shorturl.at/ZRYSp">
-`);
-const hosteriaMarker = L.marker([-26.98440183501122, -66.73915922225804]).addTo(map).bindPopup(`
-<h5>Hostería Pirucha</h5>
-`);
+    L.marker([-26.98440183501122, -66.73915922225804]).addTo(map).bindPopup(`
+        <h5>Hostería Pirucha</h5>
+    `);
+});
